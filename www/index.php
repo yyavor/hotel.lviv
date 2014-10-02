61a00406bd217bcc4a17c37508a256c306dafdd4
yyavor <yyavorovskyi@gmail.com>
2014-10-02 09:31:34 +0300

    added accordion

--- www/index.php
+++ www/index.php
@@ -7,20 +7,27 @@
     <title>Родинний двір</title>
     <link rel="stylesheet" type="text/css" href="css/style.css" />
     
+    
     <script src="third_party/jquery/jquery-1.11.1.js"></script>
     <script type="text/javascript" src="third_party/mootools/mootools-1.2-core.js"></script>
     <script type="text/javascript" src="third_party/mootools/mootools-1.2-more.js"></script>
     <script type="text/javascript" src="third_party/MooFlow/MooFlow.js"></script>
     <script type="text/javascript" src="main.js"></script>
+    <script type="text/javascript" src="/third_party/accordion/accordion.js"></script>
+    
     
 </head>
 <body>
-    
+    <!--
     <ul class="glossymenu">
 	<li id="exterier_menu_link" class="current"><a><b>Екстер'єр</b></a></li>
 	<li id="rooms_menu_link"><a><b>Номери</b></a></li>
 	<li id="contacts_menu_link"><a><b>Наші контакти</b></a></li>	
     </ul>
+    -->
+    
+    <div class="accordion"> <BR> <h3>Главное меню</h3> <BR> <p class="linked"> <BR>  <a href="http://tsa.net.ru/">Главная страница</a><br/> <BR>  <a href="http://tsa.net.ru/gb/">Гостевая книга</a><br/> <BR>  <a href="http://tsa.net.ru/index/0-3">Обратная связь</a><br/> <BR>  <a href="http://tsa.net.ru/board/">Доска объявлений</a><br/> <BR>  <a href="http://tsa.net.ru/publ/">Каталог статей</a><br/>  <BR>  <a href="http://tsa.net.ru/dir/">Каталог сайтов</a><br/> <BR>  <a href="http://tsa.net.ru/blog/">Блог</a><br/> <BR>  <a href="http://tsa.net.ru/faq/"><font color="#ff8c00">FAQ (вопрос/ответ)</font></a><br/> <BR> </p> <BR> <h3>Компьютер</h3> <BR> <p class="linked"> <BR> <a href="/load/">Каталог файлов</a><br/> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR> </p> <BR> <h3>Отдохни</h3> <BR> <p class="linked"> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/">Пусто</a><br/> <BR> </p> <BR> <h3>Форум</h3> <BR> <p class="linked"> <BR>  <a href="http://tsa.net.ru/forum/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/forum/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/forum/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/forum/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/forum/">Пусто</a><br/> <BR>  <a href="http://tsa.net.ru/forum/">Пусто</a><br/> <BR> </div> <BR> 
+
     
     <div id="content"></div>
 
---
+++ www/third_party/accordion/accordion.js
@@ -0,0 +1,43 @@
+﻿/// Accordion menu
+jQuery(document).ready(function(){
+    function createCookie(name,value,days) {
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+      }
+      else expires = "";
+      document.cookie = name+"="+value+expires+"; path=/";
+    }
+
+    function readCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      }
+      return null;
+    }
+
+    var cookier = readCookie("actr");
+    var actr = cookier ? cookier : 0;
+    var cookiel = readCookie("actl");
+    var actl = cookiel ? cookiel : 0;
+
+    jQuery(".accordion h3:eq("+actr+")").addClass("active");
+    jQuery(".accordion p:eq("+actr+")").show();
+
+    jQuery(".accordion h3").click(function(){
+        var index = jQuery(".accordion h3").index(this);
+        createCookie("actr", index, 365);
+
+        jQuery(this).next("p").slideToggle("fast")
+        .siblings("p:visible").slideUp("fast");
+        jQuery(this).toggleClass("active");
+        jQuery(this).siblings("h3").removeClass("active");
+
+    });
+
+});
